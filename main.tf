c335976c66c2c1125a2197da9d675d41fbf26009
vangalahemasree-dot <vangalahemasree@gmail.com>
2026-06-12 14:43:32 +0530

    updated

--- main.tf
+++ main.tf
@@ -38,6 +38,10 @@ resource "azurerm_subnet" "subnet" {
 
   name                 = each.value.subnet_name
   resource_group_name  = each.value.resource_group
-  virtual_network_name = each.value.vnet_name
-  address_prefixes     = each.value.address_prefixes
+
+  virtual_network_name = azurerm_virtual_network.vnet[
+    each.value.vnet_name
+  ].name
+
+  address_prefixes = each.value.address_prefixes
 }
