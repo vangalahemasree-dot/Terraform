fadc3c6a93558c2eb6ffae7b7a3fbdc9bb219d21
vangalahemasree-dot <vangalahemasree@gmail.com>
2026-06-12 14:09:16 +0530

    vnet

--- main.tf
+++ main.tf
@@ -7,3 +7,37 @@ resource "azurerm_resource_group" "rg" {
   name     = each.value.name
   location = each.value.region
 }
+resource "azurerm_virtual_network" "vnet" {
+  for_each = {
+    for vnet in var.vnets :
+    vnet.name => vnet
+  }
+
+  name                = each.value.name
+  resource_group_name = each.value.resource_group
+
+  location = azurerm_resource_group.rg[
+    each.value.resource_group
+  ].location
+
+  address_space = each.value.address_space
+}
+resource "azurerm_subnet" "subnet" {
+
+  for_each = merge([
+    for vnet in var.vnets : {
+      for subnet in vnet.subnets :
+      "${vnet.name}-${subnet.name}" => {
+        vnet_name        = vnet.name
+        resource_group   = vnet.resource_group
+        subnet_name      = subnet.name
+        address_prefixes = subnet.address_prefixes
+      }
+    }
+  ]...)
+
+  name                 = each.value.subnet_name
+  resource_group_name  = each.value.resource_group
+  virtual_network_name = each.value.vnet_name
+  address_prefixes     = each.value.address_prefixes
+}
--- variables.tf
+++ variables.tf
@@ -4,3 +4,15 @@ variable "resource_groups" {
     region = string
   }))
 }
+variable "vnets" {
+  type = list(object({
+    name          = string
+    resource_group = string
+    address_space = list(string)
+
+    subnets = list(object({
+      name             = string
+      address_prefixes = list(string)
+    }))
+  }))
+}
